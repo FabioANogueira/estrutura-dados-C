3c653f831bc3c907cc1ddb57c5f99fd145cfe656
FabioANogueira <fabioelivia500@gmail.com>
2025-07-25 20:31:38 -0300

    update 2025.07.25.2031

---
+++ aula01ex02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+
+int n1;
+int n2;
+
+std::cout << "Digite o primeiro numero: ";
+std::cin >> n1;
+std::cout << "Digite o segundo numero: ";
+std::cin >> n2;
+
+int sum = n1 + n2;
+int sub = n1 - n2;
+int mult = n1 * n2;
+int div = n1 / n2;
+float fdiv = (float)n1 / (float);
+int res = n1 % n2;
